66f482bb26f8b3d021f201f4cc25f77917c12d3d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-06 09:08:32 -0300

    Atividade Aula 01

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,28 +1,46 @@
 package br.com.fiapride.main;
+//
+//// Importamos a classe Passageiro para que o sistema a reconheça
+//import br.com.fiapride.model.Passageiro;
+//
+//public class SistemaPrincipal {
+//
+//    public static void main(String[] args) {
+//        // INSTANCIAÇÃO
+//        // O comando 'new' aloca memória para um novo objeto.
+//        // Criando o primeiro passageiro (Objeto 1)
+//        Passageiro passageiro1 = new Passageiro();
+//        passageiro1.nome = "Ana Silva";
+//        passageiro1.saldo = 50.0;
+//
+//        // Criando o segundo passageiro (Objeto 2)
+//        Passageiro passageiro2 = new Passageiro();
+//        passageiro2.nome = "Carlos Souza";
+//        passageiro2.saldo = 12.50;
+//
+//        // Exibindo os dados no Console
+//        System.out.println("--- Sistema FiapRide ---");
+//        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
+//        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
+//
+//        // Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
+//    }
+//}
 
-// Importamos a classe Passageiro para que o sistema a reconheça
-import br.com.fiapride.model.Passageiro;
+// ===ATIVIDADE AULA 01===
+import br.com.fiapride.model.Aspirador;
 
 public class SistemaPrincipal {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        // INSTANCIAÇÃO
-        // O comando 'new' aloca memória para um novo objeto.
-        // Criando o primeiro passageiro (Objeto 1)
-        Passageiro passageiro1 = new Passageiro();
-        passageiro1.nome = "Ana Silva";
-        passageiro1.saldo = 50.0;
-
-        // Criando o segundo passageiro (Objeto 2)
-        Passageiro passageiro2 = new Passageiro();
-        passageiro2.nome = "Carlos Souza";
-        passageiro2.saldo = 12.50;
-
-        // Exibindo os dados no Console
-        System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
-        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
-
-        // Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
-    }
+	Aspirador meuAspirador = new Aspirador();
+	meuAspirador.cor = "Branco";
+	meuAspirador.material = "Plástico";
+	
+	Aspirador aspiradordoProfessor = new Aspirador();
+	aspiradordoProfessor.cor = "Metálica";
+	aspiradordoProfessor.material = "Alumínio";
+	System.out.println("Meu aspirador é: " + meuAspirador.cor);
+	System.out.println("O do professor é: " + aspiradordoProfessor.cor);
+  }
 }
---
+++ src/br/com/fiapride/model/Aspirador.java
@@ -0,0 +1,8 @@
+package br.com.fiapride.model;
+public class Aspirador { // <-- Use o nome do SEU objeto    
+    // As características que você pensou    
+    public String cor;
+    public String material;
+    public int voltagem;
+    
+}
