55d06197e7a6245dfcc84856c3e2031c19d6e960
Krajner <laiskrajnerlacerda@gmail.com>
2026-03-07 18:16:17 -0300

    ATIVIDADE AULA 03

--- src/br/com/fiapride/model/Passageiro.java
+++ src/br/com/fiapride/model/Passageiro.java
@@ -1,36 +1,93 @@
+//package br.com.fiapride.model;
+//
+//public class Passageiro {
+//    public String nome;
+//    public String cpf; 
+//    public double saldo;
+//
+//    public Passageiro(String nome, String cpf) {
+//        this.nome = nome;
+//        this.cpf = cpf; 
+//        this.saldo = 0.0;
+//    }
+//
+//
+//    public void adicionarSaldo(double valor) {
+//        if (valor <= 0) {
+//            System.out.println("Erro: O valor de recarga deve ser maior que zero.");
+//            return; 
+//        }
+//        this.saldo += valor;
+//        System.out.println("Recarga realizada. Novo saldo: " + this.saldo);
+//    }
+//
+//    public void pagarViagem(double custo) {
+//        if (custo <= 0) {
+//            System.out.println("Erro: O custo da viagem é inválido.");
+//            return;
+//        }
+//        if (this.saldo < custo) {
+//            System.out.println("Erro: Saldo insuficiente para realizar a viagem.");
+//            return;
+//        }
+//        this.saldo -= custo;
+//        System.out.println("Viagem paga. Saldo restante: " + this.saldo);
+//    }
+//}
+
+//===AULA 03===
 package br.com.fiapride.model;
 
 public class Passageiro {
-    public String nome;
-    public String cpf; 
-    public double saldo;
+    private String nome;
+    private String cpf;
+    private double saldo;
+    
+public Passageiro(String nome, String cpf) {
+    this.setNome(nome);
+    this.setCpf(cpf);
+    this.setSaldo(0);
+}
 
-    public Passageiro(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf; 
-        this.saldo = 0.0;
+
+public double getSaldo() {
+    return this.saldo; 
+}
+
+private void setSaldo(double valor) {
+    if (valor >= 0) {
+        this.saldo = valor;
+    } else {
+        System.out.println("Erro de Segurança: Tentativa de definir saldo negativo bloqueada!");
     }
+}
 
 
-    public void adicionarSaldo(double valor) {
-        if (valor <= 0) {
-            System.out.println("Erro: O valor de recarga deve ser maior que zero.");
-            return; 
-        }
-        this.saldo += valor;
-        System.out.println("Recarga realizada. Novo saldo: " + this.saldo);
-    }
+public String getNome() {
+    return this.nome;
+}
 
-    public void pagarViagem(double custo) {
-        if (custo <= 0) {
-            System.out.println("Erro: O custo da viagem é inválido.");
-            return;
-        }
-        if (this.saldo < custo) {
-            System.out.println("Erro: Saldo insuficiente para realizar a viagem.");
-            return;
-        }
-        this.saldo -= custo;
-        System.out.println("Viagem paga. Saldo restante: " + this.saldo);
-    }
+private void setNome(String nome) {
+    this.nome = nome;
+}
+
+public String getCpf() {
+    return this.cpf;
+}
+
+private void setCpf(String cpf) {
+    this.cpf = cpf;
+	}
+
+
+public void pagarViagem(double d) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+public void adicionarSaldo(double d) {
+	// TODO Auto-generated method stub
+	
+}
 }
